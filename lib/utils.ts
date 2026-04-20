06cb09d0d21f027ed1b44d2dfeb0d0f58e047487
G!bulk <gibulk@yahoo.com>
2026-04-20 20:54:08 +0700

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,22 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: string | Date) {
+  return new Intl.DateTimeFormat('id-ID', {
+    dateStyle: 'long',
+  }).format(new Date(date));
+}
+
+export function slugify(text: string) {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-');
+}
