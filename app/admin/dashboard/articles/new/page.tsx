b9582fdbc8c53479e8d640f2f31581ef6abc858b
G!bulk <gibulk@yahoo.com>
2026-04-20 21:21:19 +0700

    Update page.tsx

--- app/admin/dashboard/articles/new/page.tsx
+++ app/admin/dashboard/articles/new/page.tsx
@@ -1,1 +1,10 @@
+import { ArticleForm } from '@/components/admin/ArticleForm';
 
+export default function NewArticlePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-8">Buat Artikel Baru</h1>
+      <ArticleForm />
+    </div>
+  );
+}
