603a44744303387b4f9a32faf8584cd335bacbea
G!bulk <gibulk@yahoo.com>
2026-04-20 20:57:55 +0700

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,1 +1,18 @@
+'use client';
 
+import { ThemeProvider } from 'next-themes';
+import { createClient } from '@/lib/supabase/client';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [supabaseClient] = useState(() => createClient());
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </SessionContextProvider>
+  );
+}
