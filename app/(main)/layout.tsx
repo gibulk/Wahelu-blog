e086f61f7ef27ed89c1d52989bcbc11fee0d7b5d
G!bulk <gibulk@yahoo.com>
2026-04-20 21:07:15 +0700

    Update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,1 +1,16 @@
+import { Header } from '@/components/layout/Header';
+import { Footer } from '@/components/layout/Footer';
 
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <Header />
+      <div className="flex-1">{children}</div>
+      <Footer />
+    </div>
+  );
+}
