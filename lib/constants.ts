f5e26259ca57ed5c32e099e2ae71a00d1245b48f
G!bulk <gibulk@yahoo.com>
2026-04-20 20:55:59 +0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,13 @@
+export const CATEGORIES = [
+  { value: 'geopolitik-asia-pasifik', label: 'Geopolitik Asia-Pasifik' },
+  { value: 'ekonomi-politik-global', label: 'Ekonomi-Politik Global' },
+  { value: 'web3-desentralisasi', label: 'Web3 & Desentralisasi' },
+  { value: 'konflik-perbatasan', label: 'Konflik Perbatasan' },
+];
 
+export const SITE_CONFIG = {
+  name: 'Wahelu',
+  description: 'Analisis mendalam geopolitik, geografi, dan Web3.',
+  author: 'Wahelu',
+  cryptoAddress: '0x7A92BfB2C934fC0c59c8beF75663cE76C9e2a44c', // Ganti dengan alamat dompet Anda
+};
