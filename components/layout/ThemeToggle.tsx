9277a2d97bdb5b6ec0edc61535f12ce50360642d
G!bulk <gibulk@yahoo.com>
2026-04-21 11:36:20 +0700

    Update ThemeToggle.tsx

--- components/layout/ThemeToggle.tsx
+++ components/layout/ThemeToggle.tsx
@@ -2,7 +2,6 @@
 
 import { useTheme } from 'next-themes';
 import { Button } from '../ui/Button';
-import { Moon, Sun } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 export function ThemeToggle() {
@@ -19,8 +18,7 @@ export function ThemeToggle() {
       size="sm"
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      {theme === 'dark' ? '☀️' : '🌙'}
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
