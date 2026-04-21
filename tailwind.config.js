b6e39992b493bd65b9ab10ffce8bd3fb32f177dc
G!bulk <gibulk@yahoo.com>
2026-04-21 12:27:35 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,17 @@ module.exports = {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
         },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
         muted: {
           DEFAULT: 'hsl(var(--muted))',
           foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
         },
         card: {
           DEFAULT: 'hsl(var(--card))',
